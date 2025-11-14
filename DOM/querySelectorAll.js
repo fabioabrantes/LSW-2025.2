6235488f865f67e11d2bbe817085ea575aaba111
fabioabrantes <fabio.abrantes.diniz@gmail.com>
2025-11-14 14:21:29 -0300

    add metodos selecao dom

--- DOM/querySelectorAll.js
+++ DOM/querySelectorAll.js
@@ -1,13 +1,37 @@
 const lista = document.querySelectorAll("#minhaLista li");
 const listaArray = Array.from(lista);
 
-function ordenaLista(){
-    const listaOrdenada = listaArray.sort((a,b)=>{
-        return a.textContent.localeCompare(b.textContent)
-    });
-    return listaOrdenada;
+function ordenaLista() {
+  return listaArray.sort((a, b) => a.textContent.localeCompare(b.textContent));
 }
+const listaOrdenada = ordenaLista();
 
 const ul = document.querySelector("#minhaLista");
-ul.innerHTML="";
 
+// limpa a UL
+ul.innerHTML = "";
+
+// adiciona a lista ordenada
+ordenaLista().forEach((li) => {
+  ul.appendChild(li);
+});
+
+const lista2 = document.getElementsByClassName("minhaList minhalistaAzul");
+const lista2Array = Array.from(lista2);
+console.log(lista2);
+
+
+
+
+
+/* document.querySelector("#ordenarBtn").addEventListener("click", () => {
+  const itensOrdenados = ordenaLista();
+
+  // limpa a UL
+  ul.innerHTML = "";
+
+  // adiciona novamente em ordem
+  itensOrdenados.forEach((li) => {
+    ul.appendChild(li);
+  });
+}); */
